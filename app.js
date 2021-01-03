4d79c18db2bea10e0884e39769d9c30ea6abac2d
siegelverse <msiegel37@gmail.com>
2021-01-03 14:33:46 -0600

    added error handlers for api requests

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ const weatherURL = 'http://api.weatherstack.com/current?access_key=a4788cc85c0a8
 const mapURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2llZ2VsdmVyc2UiLCJhIjoiY2tqaGl0eWRpNGxzbTJzbnA1ODZkYnd0diJ9.C85VzBtsLrEfhfgplzTmDQ'
 
 request({ url:weatherURL, json: true }, (error, response) => {
-    // console.log(response.body.current)
-    const current = response.body.current
-
-    console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees. It feels like ${current.feelslike} degrees.`)
+    if (error) {
+        console.log("Unable to connect to weather service")
+    } else if (response.body.error) {
+        console.log("Unable to find location")
+    } else {
+        const current = response.body.current
+        console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees. It feels like ${current.feelslike} degrees.`)
+    }
 })
 
 request({ url:mapURL, json: true }, (error, response) => {
+    if (error) {
+        console.log("Unable to connect to mapbox")
+    } else if (response.body.features.length === 0) {
+        console.log("Try again with different search term")
+    } else {
     const lat = response.body.features[0].center[1]
     const long = response.body.features[0].center[0]
-
     console.log(lat, long)
+    }
 })
