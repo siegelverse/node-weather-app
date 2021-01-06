7a4f5aab5506e0041bfcb59e7ed8d8edcef1bfbd
siegelverse <msiegel37@gmail.com>
2021-01-06 16:42:41 -0600

    added partials

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -34,6 +44,10 @@ app.get('/weather', (req, res) => {
         forecast: '75 degrees and sunny',
         location: 'Austin, Texas'
     })
+})
+
+app.get('*', (req, res) => {
+    res.send('404')
 })
 
 app.listen(3000, () => {
