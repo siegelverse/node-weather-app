695703e8876eb5a7c2cc7ee6d39a83fe1a8ab682
siegelverse <msiegel37@gmail.com>
2021-01-07 16:51:36 -0600

    wired up ui

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,11 +1,21 @@
 console.log('client side js file loaded')
-const url = 'http://localhost:3000/weather?address=austin'
-fetch(url).then(res => {
-    res.json().then(data => {
-        if (data.error) {
-            console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.forecastData)
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message1')
+const messageTwo = document.querySelector('#message2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    const url = 'http://localhost:3000/weather?address='+location
+    fetch(url).then(res => {
+        res.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecastData
+        })
     })
 })
