0a05003ffa1a3e54f62bc69ff7c348136ebacc78
siegelverse <msiegel37@gmail.com>
2021-01-07 16:07:58 -0600

    client side fetch

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('client side js file loaded')
+const url = 'http://localhost:3000/weather?address=austin'
+fetch(url).then(res => {
+    res.json().then(data => {
+        if (data.error) {
+            console.log(data.error)
+        }
+        console.log(data.location)
+        console.log(data.forecastData)
+    })
+})
